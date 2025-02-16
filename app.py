7469440e45668dad73b76d3c1c299140a848ef1b
Dror <dror.rosentraub@gmail.com>
2025-02-16 21:29:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -208,7 +208,7 @@ generation_config = {
 
 # Initialize Gemini model
 gemini_model = genai.GenerativeModel(
-    model_name="gemini-2.0-flash",
+    model_name="gemini-2.0-pro-exp-02-05",
     generation_config=generation_config,
 )
 
